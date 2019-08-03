3cc5df429084582469e125659f968e21b4b888a6
Joanna Kęprowska <a.keprowska@gmail.com>
2019-08-03 11:58:05 +0200

    Nowe repozytorium Student

--- savestudent/main/java/pl/sda/savestudent/App.java
+++ savestudent/main/java/pl/sda/savestudent/App.java
@@ -1,4 +1,28 @@
 package pl.sda.savestudent;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.sda.studentmodel.Student;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
+    public static void main(String[] args) {
+
+    Student student = new Student("Anna", "Kowalska", 1);
+    Student student1 = new Student("Rose", "Hathaway", 2);
+    Student student2 = new Student("Olaf", "Snow", 3);
+    Student student3 = new Student("Elza", "Moor", 4);
+    Student student4 = new Student("Lisa", "Dragomir", 5);
+        List<Student> students = Arrays.asList(student, student1, student2, student3, student4);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("students.json"), students);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
--- studentmodel/main/java/pl/sda/studentmodel/Student.java
+++ studentmodel/main/java/pl/sda/studentmodel/Student.java
@@ -1,4 +1,11 @@
 package pl.sda.studentmodel;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
 public class Student {
+
+    private String name;
+    private String lastname;
+    private Integer index;
 }
